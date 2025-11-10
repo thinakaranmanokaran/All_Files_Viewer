3e252b7722c384740ef17e17935b16e8e75e876a
thinakaranmanokaran <thinakaranmanohar@yahoo.com>
2025-11-10 16:45:14 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/All_Files_Viewer/',
   plugins: [
     react(),
     tailwindcss(),
